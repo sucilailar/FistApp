fd2e7dc393d103f24b1b4bfa521c2e7827b6354c
sucilailar <sucil445@gmail.com>
2020-09-25 18:34:08 +0700

    Push git pertama

---
+++ app/src/main/java/com/example/fistapp/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.fistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private TextView mTextView;
+    private EditText mEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.mButton);
+        mTextView = (TextView) findViewById(R.id.mTextView);
+        mEditText = (EditText) findViewById(R.id.editText);
+
+        mButton.setText(R.string.button_name);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String enterText;
+
+                enterText = mEditText.getText().toString();
+                mTextView.setVisibility(View.VISIBLE);
+//                mTextView.setText(R.string.show_me);
+                mTextView.setText(enterText);
+            }
+        });
+    }
+
+//    public void ShowMe(View view){
+//        mTextView.setVisibility(View.VISIBLE);
+//        mTextView.setText(R.string.show_me);
+//    }
+}
